75f7a11ae19b3d700d0ddddf7b2fd9eea3edfc77
Mark K Cowan <mark@battlesnake.co.uk>
2014-11-27 19:48:25 +0200

    Sync

--- src/api/config.js
+++ src/api/config.js
@@ -2,22 +2,45 @@
 	'use strict';
 
 	angular.module('battlesnake.api')
-		.constant('apiConfigs', {
-			common: {
-				secure: true,
-				path: '/'
-			},
-			dev: {
-				domain: 'api.uriel.err.ee'
-			},
-			live: {
-				domain: 'api.err.ee'
-			}
-		})
-		.constant('apiMode', 'dev')
+		.constant('apiConfigs', apiConfigs)
+		.constant('apiMode', apiMode)
 		.factory('apiConfig', apiConfig)
 		;
 
+	/**
+	 * @const apiMode
+	 *
+	 * @description
+	 * API configuration to use (is merged with 'common' configuration)
+	 */
+	var apiMode = 'dev';
+
+	/**
+	 * @const apiConfigs
+	 *
+	 * @description
+	 * API configurations
+	 */
+	var apiConfigs = {
+		common: {
+			secure: true,
+			path: '/'
+		},
+		dev: {
+			domain: 'api.uriel.err.ee'
+		},
+		live: {
+			domain: 'api.err.ee'
+		}
+	};
+
+	/**
+	 * @const apiConfig
+	 *
+	 * @description
+	 * Merges the selected configuration mode (apiConfigs[apiMode]) with the
+	 * common configuration (apiConfigs.common) and returns it
+	 */
 	function apiConfig(apiConfigs, apiMode) {
 		return $({}).extend(apiConfigs.common, apiConfigs[apiMode]);
 	}
